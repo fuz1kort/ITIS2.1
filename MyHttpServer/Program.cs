ab301bda8e498446f691e40657c849ab2e2fd9f4
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-09-30 00:27:50 +0300

    go sleep dude

--- MyHttpServer/Program.cs
+++ MyHttpServer/Program.cs
@@ -9,44 +9,6 @@ class Program
     private const string PATH_CONFIGURATION = "appsettings.json";
     static async Task Main()
     {
-        // try
-        // {
-        //     if (!File.Exists(PATH_CONFIGURATION))
-        //     {
-        //         Console.WriteLine("Файл appsettings.json не найден");
-        //         throw new FileNotFoundException();
-        //     }
-        //     
-        //     var strJson = await File.OpenText(PATH_CONFIGURATION).ReadToEndAsync();
-        //     var config = JsonConvert.DeserializeObject<AppSettingsConfig>(strJson);
-        //     
-        //     
-        //      using var httpListener = new HttpListener();
-        //      httpListener.Prefixes.Add($"http://{config.Address}:{config.Port}/");
-        //      httpListener.Start();
-        //      Console.WriteLine("Serves has started");
-        //     
-        //      while (true)
-        //      {
-        //          var context = await httpListener.GetContextAsync();
-        //          context.Response.ContentType = "text/html; charset=utf-8";
-        //          using var response = context.Response;
-        //     
-        //          var buffer = await File.ReadAllBytesAsync("index.html");
-        //          response.ContentLength64 = buffer.Length;
-        //          
-        //          await using var output = response.OutputStream;
-        //     
-        //          await output.WriteAsync(buffer);
-        //          await output.FlushAsync();
-        //      }
-        // }
-        // catch (FileNotFoundException e)
-        // {
-        //     Console.WriteLine(e);
-        //     throw;
-        // }
-
         var server = new HttpServer();
         await server.Start();
     }   

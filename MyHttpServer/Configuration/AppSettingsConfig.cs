7e34f44d63a4aa8c5fc13e3a8d9b6ca660979c2a
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-09-30 22:13:17 +0300

    откат до 15.09

--- MyHttpServer/Configuration/AppSettingsConfig.cs
+++ MyHttpServer/Configuration/AppSettingsConfig.cs
@@ -5,28 +5,5 @@ public class AppSettingsConfig
     public string Port { get; private set; }
     public string Address { get; private set; }
     public string StaticPathFiles { get; private set; }
-    public string MailSender { get; private set; }
-    public string PasswordSender { get; private set; }
-    public string ToEmail { get; private set; }
-    public string SmtpServerHost { get; private set; }
-    public ushort SmtpServerPort { get; private set; }
-
-    public AppSettingsConfig(string port = "",
-        string address = "",
-        string staticFilesPath = "",
-        string mailSender = "",
-        string passwordSender = "",
-        string toEmail = "",
-        string smtpServerHost = "",
-        ushort smtpServerPort = 0)
-    {
-        Port = port;
-        Address = address;
-        StaticPathFiles = staticFilesPath;
-        MailSender = mailSender;
-        PasswordSender = passwordSender;
-        ToEmail = toEmail;
-        SmtpServerHost = smtpServerHost;
-        SmtpServerPort = smtpServerPort;
-    }
+    
 }
--- MyHttpServer/Program.cs
+++ MyHttpServer/Program.cs
@@ -1,16 +1,37 @@
-﻿using System.Diagnostics.Tracing;
-using System.Net;
+﻿using System.Net;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Nodes;
-using System.Xml.Schema;
-using MyHttpServer;
+ 
+HttpListener server = new HttpListener();
+// установка адресов прослушки
+server.Prefixes.Add("http://127.0.0.1:2323/");
+server.Start(); // начинаем прослушивать входящие подключения
+Console.WriteLine("Сервер запущен");
 
-public class Program
+while (true)
 {
-    public static async Task Main(string[] args)
-    {
-        var server = new ServerHandler();
-        await server.Start();
-    }
+    // получаем контекст
+    var context = await server.GetContextAsync();
+    var response = context.Response;
+    var request = context.Request;
+    var filePath = "C:\\Users\\Marat\\source\\repos\\fuz1kort\\ITIS2.1\\MyHttpServer\\static\\battlenet.html";
+    var fileContent = File.ReadAllText(filePath);
+    Console.WriteLine("Запрос обработан");
+// отправляемый в ответ код htmlвозвращает
+    byte[] buffer = Encoding.UTF8.GetBytes(fileContent);
+// получаем поток ответа и пишем в него ответ
+    response.ContentLength64 = buffer.Length;
+    await using Stream output = response.OutputStream;
+// отправляем данные
+    await output.WriteAsync(buffer);
+    await output.FlushAsync();
+
+    if (Console.ReadLine() == "stop")
+        break;
 }
+
+
+ 
+Console.WriteLine("Запрос обработан");
+Console.WriteLine("Сервер прекратил работу");
+ 
+server.Stop();

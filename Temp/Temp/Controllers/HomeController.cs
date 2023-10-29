bcfea922d213360088a56775b0375248ae93df26
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-10-29 13:19:04 +0300

    added calculator to temp

---
+++ Temp/Temp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Temp.Extensions;
+using Temp.Models;
+
+namespace Temp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Calculator()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Calculate(string first, string operation, string second)
+    {
+        var result = CalculatorExtension.Calculate(first, operation, second);
+
+        return View(result);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
